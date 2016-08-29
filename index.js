ed6ceeff740fbb0330e945374e6acd9c104a71eb
Igor Maliy <imaliy.filsh@gmail.com>
2016-08-29 23:08:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function sendCodeToKuzzle(strategy, oauthWindow, cb, kuzzle) {
 
     if (code) {
       oauthWindow.close();
-      kuzzle.query({controller: 'auth', action: 'login'}, {body: {strategy, code: c[1]}}, (err, res) => {
+      kuzzle.query({controller: 'auth', action: 'login'}, {body: {strategy, code: code[1]}}, (err, res) => {
         if (err) {
           cb(err);
           return;
